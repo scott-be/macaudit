6ad97368bfc3cc452047ed50f28f82254e32ab35
scott <sb1975@gmail.com>
2014-12-09 22:24:33 -0500

    printf
    replaced echo with printf

--- macaudit.sh
+++ macaudit.sh
@@ -2,30 +2,31 @@
 
 OUTPUT_FILE=out.txt
 
-printf > $OUTPUT_FILE
+printf '' > $OUTPUT_FILE
 
 # echo "" $() | tee -a $OUTPUT_FILE
 
 #==============#
 # Date of Scan #
 #==============#
-echo "Date:" $(date) | tee -a $OUTPUT_FILE
+printf "Date: %s\n" date | tee -a $OUTPUT_FILE
 
 #===============#
 # Computer Name #
 #===============#
-echo "Hostname:" $(hostname) | tee -a $OUTPUT_FILE
+printf "Hostname: %s\n" hostname | tee -a $OUTPUT_FILE
 
 #============#
 # OS Version #
 #============#
-echo "OS Version:" $(sw_vers -productVersion) | tee -a $OUTPUT_FILE
+printf "OS Version: %s\n" sw_vers -productVersion | tee -a $OUTPUT_FILE
 
 #================#
 # Unique user ID #
 #================#
-echo "Username:" $(whoami) | tee -a $OUTPUT_FILE
-echo "Full Name:" $(finger `whoami` | awk -F: '{ print $3 }' | head -n1 | sed 's/^ //') | tee -a $OUTPUT_FILE
+printf "Username: %s\n" whoami | tee -a $OUTPUT_FILE
+FULL_NAME=`finger \`whoami\` | awk -F: '{ print $3 }' | head -n1 | sed 's/^ //'`
+printf "Full Name: %s\n" $FULL_NAME | tee -a $OUTPUT_FILE
 
 #===================#
 # Autologon Enabled #
@@ -34,24 +35,30 @@ AUTOLOGON_USER=`defaults read /Library/Preferences/com.apple.loginwindow autoLog
 	if [ $? -eq 0 ]; then
 		printf "Autologon: Enabled with user '%s'\n" $AUTOLOGON_USER | tee -a $OUTPUT_FILE
 	else
-		echo "AutoLogon: Disbaled" | tee -a $OUTPUT_FILE
+		printf "AutoLogon: Disbaled\n" | tee -a $OUTPUT_FILE
 	fi
 
 #======================#
 # Screen Saver Enabled #
 #======================#
-echo "Screen Saver:" $(if [ $(defaults -currentHost read com.apple.screensaver idleTime -int) -eq 0 ]; then echo "Disabled"; else echo "Enabled"; fi) | tee -a $OUTPUT_FILE
+# echo "Screen Saver:" $(if [ $(defaults -currentHost read com.apple.screensaver idleTime -int) -eq 0 ]; then echo "Disabled"; else echo "Enabled"; fi) | tee -a $OUTPUT_FILE
+if [ $(defaults -currentHost read com.apple.screensaver idleTime -int) -eq 0 ]; then
+	printf "Screen Saver: Disabled\n" | tee -a $OUTPUT_FILE
+else
+	printf "Screen Saver: Enabled\n" | tee -a $OUTPUT_FILE
+fi
+
 
 #======================#
 # Screen Saver Timeout #
 #======================#
-echo "Screen Saver Timeout:" $(defaults -currentHost read com.apple.screensaver idleTime -int) | tee -a $OUTPUT_FILE
+printf "Screen Saver Timeout: %s\n" "`defaults -currentHost read com.apple.screensaver idleTime -int`" | tee -a $OUTPUT_FILE
 
 #=================================#
 # Screen Saver Password Protected #
 #=================================#
-echo "Ask For Password:" $(defaults read com.apple.screensaver askForPassword -int) | tee -a $OUTPUT_FILE
-echo "Ask For Password Delay:" $(defaults read com.apple.screensaver askForPasswordDelay -int) | tee -a $OUTPUT_FILE
+printf "Ask For Password: %s\n" "`defaults read com.apple.screensaver askForPassword -int`" | tee -a $OUTPUT_FILE
+printf "Ask For Password Delay: %s\n" "`defaults read com.apple.screensaver askForPasswordDelay -int`" | tee -a $OUTPUT_FILE
 
 #======================#
 # Force Network Logoff #
@@ -101,7 +108,7 @@ echo "Ask For Password Delay:" $(defaults read com.apple.screensaver askForPassw
 #======================#
 # Hard Drive Encrypted #
 #======================#
-echo "HDD Encryption:" $(fdesetup status) | tee -a $OUTPUT_FILE
+printf "HDD Encryption: %s\n" "`fdesetup status`" | tee -a $OUTPUT_FILE
 
 #===============#
 # USB Encrypted #
